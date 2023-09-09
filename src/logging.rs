8a25e9356c82fae3d64d8701e9fe0d4a67b23b67
miampf <miampf@proton.me>
2023-09-09 20:33:03 +0200

    control over logging verbosity

--- src/logging.rs
+++ src/logging.rs
@@ -11,8 +11,15 @@ fn setup_logger_color_scheme() -> ColoredLevelConfig {
         .trace(Color::BrightBlack)
 }
 
-fn setup_stdio_logging() -> Result<fern::Dispatch, anyhow::Error> {
+fn setup_stdio_logging(verbosity: u8) -> Result<fern::Dispatch, anyhow::Error> {
     let colors = setup_logger_color_scheme();
+
+    let filter = match verbosity {
+        0 => log::LevelFilter::Warn,
+        1 => log::LevelFilter::Info,
+        2 => log::LevelFilter::Debug,
+        _ => log::LevelFilter::Trace,
+    };
 
     let dispatch = fern::Dispatch::new()
         .format(move |out, message, record| {
@@ -24,7 +31,7 @@ fn setup_stdio_logging() -> Result<fern::Dispatch, anyhow::Error> {
                 message
             ))
         })
-        .level(log::LevelFilter::Warn)
+        .level(filter)
         .chain(std::io::stdout());
 
     Ok(dispatch)
@@ -41,17 +48,19 @@ fn setup_file_logging() -> Result<fern::Dispatch, anyhow::Error> {
                 message
             ))
         })
-        .level(log::LevelFilter::Debug)
+        .level(log::LevelFilter::Trace)
         .chain(fern::log_file(get_app_directory()? + "/bote.log")?);
     Ok(dispatch)
 }
 
-pub fn setup_logger() -> Result<(), anyhow::Error> {
+pub fn setup_logger(verbosity: u8) -> Result<(), anyhow::Error> {
+    let verbosity = verbosity.clamp(0, 3);
+
     let dispatch = fern::Dispatch::new();
 
     dispatch
         .chain(setup_file_logging()?)
-        .chain(setup_stdio_logging()?)
+        .chain(setup_stdio_logging(verbosity)?)
         .apply()?;
 
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
 use std::sync::Arc;
 
-use bote::config::config_callback;
+use bote::config::{config_callback, get_app_directory};
 use bote::logging;
+use clap::{Parser, Subcommand};
 use log::info;
 use veilid_core::VeilidUpdate;
+
+#[derive(Parser)]
+#[command(
+    author = "miampf",
+    about = "A package manager build upon the veilid network."
+)]
+struct Cli {
+    #[arg(long, help = "Delete the old log file")]
+    clear_log_file: bool,
+    #[arg(short, help = "Increase the verbosity of the output (maximum is -vvv)", action = clap::ArgAction::Count)]
+    verbosity: u8,
+}
 
 fn update_callback(_update: VeilidUpdate) {}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    logging::setup_logger()?;
+    let cli = Cli::parse();
+
+    logging::setup_logger(cli.verbosity)?;
+
+    if cli.clear_log_file {
+        std::fs::remove_file(get_app_directory()? + "/bote.log")?;
+    }
 
     let update_callback = Arc::new(update_callback);
     let config_callback = Arc::new(config_callback);
