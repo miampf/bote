86e68ef6e3c6da7ec732d1f63936ec20fdafe17a
miampf <miampf@proton.me>
2023-09-10 18:14:27 +0200

    sketch of functions a first iteration of a build script will need

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,3 +1,5 @@
+mod buildscript;
+
 /// run() runs the install subcommand which is used to install a package.
 pub fn run() {
     todo!() // TODO: implement the install subcommand
---
+++ src/commands/install/buildscript.rs
@@ -0,0 +1,31 @@
+use rhai::{Engine, ImmutableString};
+
+/// setup_rhai_engine() registers all functions a build script can use for the given engine.
+pub fn setup_rhai_engine(engine: &mut Engine) {
+    engine
+        .register_fn("clone_git_repo", clone_git_repo)
+        .register_fn("execute_system_command", execute_system_command)
+        .register_fn("download_file", download_file)
+        .register_fn("change_working_directory", change_working_directory);
+}
+
+/// clone_git_repo() clones a git repository to the working directory.
+fn clone_git_repo(repo: ImmutableString) {
+    todo!()
+}
+
+/// execute_system_command() executes a system command in the current working directory.
+fn execute_system_command(cmd: ImmutableString) {
+    todo!()
+}
+
+/// download_file() downloads a file from a given URL to the current working directory.
+fn download_file(url: ImmutableString) {
+    todo!()
+}
+
+/// change_working_directory() changes the working directory to a new path. This path can be
+/// relative to the current working directory.
+fn change_working_directory(path: ImmutableString) {
+    todo!()
+}
